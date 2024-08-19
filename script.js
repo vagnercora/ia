285ed3df15e3af6be5cf9a76018a536ec5ac718f
vagnercora <160506431+vagnercora@users.noreply.github.com>
2024-08-19 11:51:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const perguntas = [
         enunciado: "O desemprego é um problema crescente em muitas economias? ",
         alternativas: [
             {
-                texto: "Sim, o desemprego está aumentando em várias economias.",
-                afirmação: "O ensino religioso no Brasil será mais inclusivo e diversificado.",
+                texto: "Sim, o desemprego está aumentando em várias economias.1",
+                afirmacao: "O ensino religioso no Brasil será mais inclusivo e diversificado.",
             },
             {
                 texto: "Não, o ensino religioso no Brasil permanecerá restrito a poucas religiões.",
-                afirmação: "O ensino religioso no Brasil permanecerá restrito a poucas religiões.",
+                afirmacao: "O ensino religioso no Brasil permanecerá restrito a poucas religiões.",
             }
         ]
     },
@@ -23,11 +23,11 @@ const perguntas = [
         alternativas: [
             {
                 texto:  "Sim, as escolas brasileiras integrarão a tecnologia nas aulas de ensino religioso.",
-                afirmação: "As escolas brasileiras integrarão a tecnologia nas aulas de ensino religioso."
+                afirmacao: "As escolas brasileiras integrarão a tecnologia nas aulas de ensino religioso."
             },
             {
                 texto: "Não, as escolas brasileiras não integrarão a tecnologia nas aulas de ensino religioso.",
-                afirmação: "As escolas brasileiras não integrarão a tecnologia nas aulas de ensino religioso."
+                afirmacao: "As escolas brasileiras não integrarão a tecnologia nas aulas de ensino religioso."
             }
         ]
 
@@ -37,11 +37,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, haverá um maior diálogo inter-religioso nas escolas brasileiras",
-                afirmação: "Haverá um maior diálogo inter-religioso nas escolas brasileiras."
+                afirmacao: "Haverá um maior diálogo inter-religioso nas escolas brasileiras."
             },
             {
                 texto: "Não, não haverá um maior diálogo inter-religioso nas escolas brasileiras.",
-                afirmação: "Não haverá um maior diálogo inter-religioso nas escolas brasileiras."
+                afirmacao: "Não haverá um maior diálogo inter-religioso nas escolas brasileiras."
             }
         ]
     },
@@ -50,11 +50,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, o ensino religioso contribuirá para a formação de cidadãos mais tolerantes e respeitosos.",
-                afirmação: "O ensino religioso contribuirá para a formação de cidadãos mais tolerantes e respeitosos."
+                afirmacao: "O ensino religioso contribuirá para a formação de cidadãos mais tolerantes e respeitosos."
             },
             {
                 texto: "Não, o ensino religioso não contribuirá para a formação de cidadãos mais tolerantes e respeitosos.",
-                afirmação: "O ensino religioso não contribuirá para a formação de cidadãos mais tolerantes e respeitosos."
+                afirmacao: "O ensino religioso não contribuirá para a formação de cidadãos mais tolerantes e respeitosos."
             }
         ]
     },
@@ -63,15 +63,16 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, as políticas públicas incentivarão a inclusão do ensino religioso no currículo escolar.",
-                afirmação: "As políticas públicas incentivarão a inclusão do ensino religioso no currículo escolar."
+                afirmacao: "As políticas públicas incentivarão a inclusão do ensino religioso no currículo escolar."
             },
             {
                 texto: "Não, as políticas públicas não incentivarão a inclusão do ensino religioso no currículo escolar.",
-                afirmação: "As políticas públicas não incentivarão a inclusão do ensino religioso no currículo escolar."
+                afirmacao: "As políticas públicas não incentivarão a inclusão do ensino religioso no currículo escolar."
             }
         ]
     },
 ];
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
@@ -81,7 +82,7 @@ function mostraPergunta() {
         mostraResultado();
         return;
       }
-      perguntaAtual = perguntas[atual]
+      perguntaAtual = perguntas[atual];
       caixaPerguntas.textContent = perguntaAtual.enunciado;
       caixaAlternativas.textContent = "";
       mostraAlternativas();
@@ -99,14 +100,15 @@ function mostraAlternativas(){
 
 function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = opcaoSelecionada.afirmação
-    historiaFinal += afirmacoes = " ";
+    historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
 
 function motraResultado() {
-    caixaPerguntas.textContent = "escreva aq";
+    caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
+
 mostraPergunta();
