a6b9482b89c582b0faab5c6e74dc4aa9e365cd58
vagnercora <160506431+vagnercora@users.noreply.github.com>
2024-08-21 20:32:02 -0300

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-reultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
@@ -78,34 +78,33 @@ let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta() {
-      if (atual >= perguntaAtual.lenght) {
+    if (atual >= perguntas.length) {
         mostraResultado();
         return;
-      }
-      perguntaAtual = perguntas[atual];
-      caixaPerguntas.textContent = perguntaAtual.enunciado;
-      caixaAlternativas.textContent = "";
-      mostraAlternativas();
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
-function mostraAlternativas(){
+function mostraAlternativas() {
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
-
     }
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmação
+    const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
 
-function motraResultado() {
+function mostraResultado() {
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
